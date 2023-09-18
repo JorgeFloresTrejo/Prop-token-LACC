feaa115fa2b5f237c982aa2d27f6d2a597db3a47
m4rtinjoel <m4rtinjl@gmail.com>
2023-09-18 13:39:59 -0600

    Agregar las urls y mostrar los botones de walletConnect y metamask

--- frontend/src/components/SessionMenu/SessionMenu.jsx
+++ frontend/src/components/SessionMenu/SessionMenu.jsx
@@ -1,10 +1,43 @@
-
+import React, { useState } from "react";
 import "./SessionMenu.css";
 import Loga from "../../images/logo1.png";
-
+import WalletConnect from "../WalletConnect/WalletConnect";
+import Web3 from "web3";
 
 const SessionMenu = () => {
-
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [Metamask, setMetamask] = useState(false);
+  const [account, setAccount] = useState("");
+  const [balance, setBalance] = useState(null);
+  const [walletDetallesVisible, setWalletDestallesVisible] = useState(false);
+  //Metamask
+  const conectarWallet = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const web3Instance = new Web3(window.ethereum);
+      //     /**----------------------------------------- */
+      try {
+        await window.ethereum.enable();
+        //obteniendo la cuenta
+        const accounts = await web3Instance.eth.getAccounts();
+        setAccount(accounts[0]);
+        //obtener el saldo de la cuenta
+        const balanceWei = await web3Instance.eth.getBalance(accounts[0]);
+        const balanceEth = web3Instance.utils.fromWei(balanceWei, "ether");
+        setBalance(balanceEth);
+      } catch (e) {
+        console.log("error: ", e);
+      }
+    } else {
+      setMetamask(false);
+    }
+  };
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+  //Mostrar detalles de la cuenta
+  const walletDetalles = () => {
+    setWalletDestallesVisible(!walletDetallesVisible);
+  };
   return (
     <div className="container">
       <div className="row">
@@ -17,6 +50,18 @@ const SessionMenu = () => {
               </h3>
             </div>
             <div className="connection-wallet">
+              <div className="connect-wallet">
+                <WalletConnect />
+              </div>
+              <button onClick={conectarWallet} className="metamask-connect">
+                <i>
+                  {account
+                    ? account.slice(0, 5) +
+                      "..." +
+                      account.slice(account.length - 4, account.length)
+                    : "Metamask"}
+                </i>
+              </button>
             </div>
           </div>
         </div>
@@ -27,19 +72,16 @@ const SessionMenu = () => {
             <div className="desplegable">
               <button className="menu-button"> Crear Token</button>
               <button className="menu-button">Inmuebles</button>
-              <button className="menu-button">
-                Wallets
-              </button>
+              <button className="menu-button">Wallets</button>
               <button className="menu-button">Mis activos</button>
               <button className="menu-button">configuracion</button>
               <button className="menu-button">Cerrar Session</button>
             </div>
           </div>
-          <div className="tercer-div">
-            </div>
-          </div>
+          <div className="tercer-div"></div>
         </div>
       </div>
+    </div>
   );
 };
 
--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,39 +1,73 @@
 import { Link } from "react-router-dom";
 const Login = () => {
-
   return (
-   <>
-          <div>
-            <h1>Invierte en inmuebles tokenizados</h1>
+    <>
+      <div>
+        <h1>Invierte en inmuebles tokenizados</h1>
+      </div>
+
+      <div className="mt-20 md:mt-2 shadow-xl px-5 py-4 rounded-xl bg-white">
+        <h1 className="text-indigo-700 text-center mx-20 font-black text-3xl">
+          Inicia sesión
+        </h1>
+
+        <form action="/Inicio" className="mt-5">
+          <div className="my-7">
+            <label
+              htmlFor="email"
+              className="uppercase text-gray-700 text-2xl block font-bold"
+            >
+              {" "}
+              email{" "}
+            </label>
+            <input
+              type="email"
+              placeholder="Email de registro"
+              id="email"
+              className="border w-full p-2 mt-3 bg-gray-200 rounded-xl"
+            />
           </div>
 
-          <div className="mt-20 md:mt-2 shadow-xl px-5 py-4 rounded-xl bg-white">
-            <h1 className="text-indigo-700 text-center mx-20 font-black text-3xl">
-                Inicia sesión
-            </h1>
-              
-            <form action="/Inicio" className="mt-5">
-              <div className="my-7">
-                <label htmlFor="email"  className="uppercase text-gray-700 text-2xl block font-bold"> email </label>
-                <input type="email" placeholder="Email de registro" id="email" className="border w-full p-2 mt-3 bg-gray-200 rounded-xl"/>
-              </div>
-
-              <div className="my-7">
-                <label htmlFor="pssword"  className="uppercase text-gray-600 text-2xl block font-bold"> contraseña </label>
-                <input type="password" placeholder="Tu contraseña" id="password" className="border w-full p-2 mt-3 bg-gray-200 rounded-xl"/>
-              </div>
-
-              <input type="submit" value="Iniciar sesión" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-900 md:w-auto"/>
-            </form>
-
-            <nav className="mt-5 lg:flex justify-between">
-              <Link to="/registrar" className="block text-center my-5 text-gray-600">¿No tienes una cuenta? Registrate</Link>
-              <Link to="/olvide-password" className="block text-center my-5 text-gray-600">Olvide mi contraseña</Link>
-            </nav>
-
+          <div className="my-7">
+            <label
+              htmlFor="pssword"
+              className="uppercase text-gray-600 text-2xl block font-bold"
+            >
+              {" "}
+              contraseña{" "}
+            </label>
+            <input
+              type="password"
+              placeholder="Tu contraseña"
+              id="password"
+              className="border w-full p-2 mt-3 bg-gray-200 rounded-xl"
+            />
           </div>
-   </>
-  )
-}
 
-export default Login
+          <input
+            type="submit"
+            value="Iniciar sesión"
+            className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-900 md:w-auto"
+          />
+        </form>
+
+        <nav className="mt-5 lg:flex justify-between">
+          <Link
+            to="/login/registrar"
+            className="block text-center my-5 text-gray-600"
+          >
+            ¿No tienes una cuenta? Registrate
+          </Link>
+          <Link
+            to="/olvide-password"
+            className="block text-center my-5 text-gray-600"
+          >
+            Olvide mi contraseña
+          </Link>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Login;
