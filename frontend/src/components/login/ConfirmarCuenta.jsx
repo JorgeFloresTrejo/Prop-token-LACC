40dde400a4d5020c4292cde4e5766dbc9756f58c
Jorge Flores <jorge.flores2020@hotmail.com>
2023-09-17 19:03:05 -0600

    Enviando email con el enlace para confirmar cuenta utilizando mailtrap

--- frontend/src/components/login/ConfirmarCuenta.jsx
+++ frontend/src/components/login/ConfirmarCuenta.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {useParams} from 'react-router-dom';
 
 const ConfirmarCuenta = () => {
+
+  
+  const params = useParams();
+
+  console.log(params);
+
   return (
+
     <>
-        <h1>Desde confirmar cuenta</h1>
+          <div>
+            <h1>
+              RConfirma tu cuenta y empieza a invertir
+            </h1>
+        </div>
+
+   
+        <div className="mt-20 md:mt-1 shadow-xl px-5 py-4 rounded-xl bg-white">
+         
+         
+
+        </div>
+
     </>
   )
 }
