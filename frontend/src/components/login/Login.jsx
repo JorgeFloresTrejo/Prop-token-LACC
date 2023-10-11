b5bca392f4d66b067fcfd0721ee1d9fc8db01769
m4rtinjoel <m4rtinjl@gmail.com>
2023-10-11 13:33:54 -0600

    google

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -56,7 +56,7 @@ const Login = () => {
             <p class="mb-0 mr-4 text-lg font-bold">Inicia sesión con</p>
             {/* <!-- Gmail --> */}
             <button
-              onClick={cargarGoogle()}
+              onClick={cargarGoogle}
               class="flex items-center mb-2 justify-center transition ease-in-out delay-50 px-3 py-2.5 space-x-2 bg-blue-200 border border-slate-600 rounded-md hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600 focus:ring-opacity-50"
             >
               <svg
